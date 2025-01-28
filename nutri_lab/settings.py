56f216805fdf9c9660d07fb6066b4b190f3af2fe
Odisley Ribeiro da Silva <121490593+Odisley37@users.noreply.github.com>
2025-01-27 22:04:03 -0300

    Commit da aplicação do dia, aprimoramento das funções e criação de models e app plataforma.

--- nutri_lab/settings.py
+++ nutri_lab/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # apps
     'autenticacao',
+    'plataforma',
 ]
 
 MIDDLEWARE = [
---
+++ plataforma/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Pacientes, DadosPaciente
+
+
+admin.site.register(Pacientes)
+admin.site.register(DadosPaciente)
+
+
